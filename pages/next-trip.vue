c21b536b0b8edc277d209bd099ebf7747c7e1aac
Gabriel Gripp <550632+grippado@users.noreply.github.com>
2024-05-30 23:56:21 -0300

    adjusting css and logical inject

--- pages/next-trip.vue
+++ pages/next-trip.vue
@@ -1,14 +1,6 @@
 <template lang="pug">
 section.ctdn-bg.sec-wh
-  #countdown.countdown
-    .counting-value.days
-    .counting-size days
-    .counting-value.hours
-    .counting-size hours
-    .counting-value.minutes
-    .counting-size min 
-    .counting-value.seconds
-    .counting-size sec
+  .countdown
 </template>
 
 <script setup lang="ts">
@@ -16,52 +8,48 @@ import { onMounted } from 'vue'
 const title = ref('gripp.link | Next Trip Date')
 
 onMounted(() => {
-  const countdownElement = document.getElementById('countdown')
-  const countElementDays = document.querySelector('.counting-value.days')
-  const countElementHours = document.querySelector('.counting-value.hours')
-  const countElementMinutes = document.querySelector('.counting-value.minutes')
-  const countElementSeconds = document.querySelector('.counting-value.seconds')
-  const end = new Date('June 12, 2024 22:45:00')
+  const countdownElement = document.querySelector('.countdown')
   const _second: number = 1000
   const _minute: number = _second * 60
   const _hour: number = _minute * 60
   const _day: number = _hour * 24
-  let timer: string | number | any | undefined
+  let timer: any
 
   function showRemaining () {
-    let now = new Date();
-    let distance = end.getTime() - now.getTime();
-    if (distance < 0) {
-
-      clearInterval(timer);
-      if (countdownElement) {
-        countdownElement.innerHTML = 'Aguarde a próxima viagem!';
-      }
-
-      return;
-    }
+    let distance = new Date('June 12, 2024 22:45:00').getTime() - new Date().getTime()
     let days = Math.floor(distance / _day)
     let hours = Math.floor((distance % _day) / _hour) < 10 ? '0' + Math.floor((distance % _day) / _hour) : Math.floor((distance % _day) / _hour)
     let minutes = Math.floor((distance % _hour) / _minute) < 10 ? '0' + Math.floor((distance % _hour) / _minute) : Math.floor((distance % _hour) / _minute)
     let seconds = Math.floor((distance % _minute) / _second) < 10 ? '0' + Math.floor((distance % _minute) / _second) : Math.floor((distance % _minute) / _second)
     
-    if (countdownElement) {
-      if (countElementDays) {
-        countElementDays.innerHTML = `${days}`
-      }
-      if (countElementHours) {
-        countElementHours.innerHTML = `${hours}`
-      }
-      if (countElementMinutes) {
-        countElementMinutes.innerHTML = `${minutes}`
-      }
-      if (countElementSeconds) {
-        countElementSeconds.innerHTML = `${seconds}`
+    if (distance < 0) {
+      clearInterval(timer)
+      if (countdownElement) {
+        countdownElement.innerHTML = 'Aguarde a próxima viagem!'
       }
     }
+
+    if (countdownElement) {
+      countdownElement.innerHTML = `<div class="counting-column">
+                                      <div class="counting-value">${days}</div>
+                                      <div class="counting-size">days</div>
+                                    </div>
+                                    <div class="counting-column">
+                                      <div class="counting-value">${hours}</div>
+                                      <div class="counting-size">hours</div>
+                                    </div>
+                                    <div class="counting-column">
+                                      <div class="counting-value">${minutes}</div>
+                                      <div class="counting-size">min</div>
+                                    </div>
+                                    <div class="counting-column">
+                                      <div class="counting-value">${seconds}</div>
+                                      <div class="counting-size">sec</div>
+                                    </div>`
+    }
   }
-  // showRemaining()
-  timer = setInterval(showRemaining, 1000)
+  showRemaining()
+  // timer = setInterval(showRemaining, 1000)
 
   const backgroundImages: Array<string> = [
     '/praca-da-liberdade.jpeg',
@@ -80,7 +68,6 @@ onMounted(() => {
   }
 
   setInterval(randomizeBg, 10000)
-
 })
 
 useSeoMeta({
@@ -111,31 +98,41 @@ useSeoMeta({
     z-index 333
   .countdown
     display flex
-    justify-content center
-    align-items baseline
     z-index 666
     font-size 6rem
     margin-top -40px
-    .counting-size
-      font-size 2rem
+    :deep(.counting-column)
+      display flex
+      flex-direction column
+      width 120px
+      line-height 50px
+    :deep(.counting-size)
+      font-size 1.33rem
+      letter-spacing -1px
       color rgba(#FFF, 0.6)
-    .counting-value
+      text-align center
+    :deep(.counting-value)
       font-size 6rem
-      width 100px
-      height 85px
+      letter-spacing -3px
       color rgba(#FFF, 0.9)
+      text-align center
+
+// LIGHT MODE
 body.light
   .ctdn-bg
     &:before
       background rgba(211, 211, 211, 0.66)
   .countdown
-    .counting-size
-      color rgba(#282a36, 0.6)
-    .counting-value
+    :deep(.counting-size),
+    :deep(.counting-value)
       color rgba(#282a36, 0.9)
+
+// MEDIA QUERIES
 @media (max-width: 500px)
   .ctdn-bg
     .countdown
       flex-direction column
       align-items center
+      :deep(.counting-column)
+        margin 10px 0
 </style>
