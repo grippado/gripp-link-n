4e191896ee177bb1a73fe37b2ecb66091a6cafea
Gabriel Gripp <550632+grippado@users.noreply.github.com>
2024-06-06 09:59:02 -0300

    split components

---
+++ components/countdown-clock.vue
@@ -0,0 +1,123 @@
+<template lang="pug">
+.ctdn-wrapper
+  div(:class="`${wrapperClass} countdown`")
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+const props = defineProps<{
+  title: string
+  date: string,
+  wrapperClass?: string
+}>()
+
+const title = ref(props.title)
+
+onMounted(() => {
+  const countdownElement = document.querySelector(`.${props.wrapperClass}`)
+  const _second: number = 1000
+  const _minute: number = _second * 60
+  const _hour: number = _minute * 60
+  const _day: number = _hour * 24
+  let timer: any
+
+  function showRemaining () {
+    let distance = new Date(props.date).getTime() - new Date().getTime()
+    let days = Math.floor(distance / _day)
+    let hours = Math.floor((distance % _day) / _hour) < 10 ? '0' + Math.floor((distance % _day) / _hour) : Math.floor((distance % _day) / _hour)
+    let minutes = Math.floor((distance % _hour) / _minute) < 10 ? '0' + Math.floor((distance % _hour) / _minute) : Math.floor((distance % _hour) / _minute)
+    let seconds = Math.floor((distance % _minute) / _second) < 10 ? '0' + Math.floor((distance % _minute) / _second) : Math.floor((distance % _minute) / _second)
+
+    if (distance < 0) {
+      clearInterval(timer)
+      if (countdownElement) {
+        countdownElement.innerHTML = 'Aguarde a próxima viagem!'
+      }
+    }
+
+    if (countdownElement) {
+      countdownElement.innerHTML = `<div class="counting-column">
+                                      <div class="counting-value">${days}</div>
+                                      <div class="counting-size">days</div>
+                                    </div>
+                                    <div class="counting-column">
+                                      <div class="counting-value">${hours}</div>
+                                      <div class="counting-size">hours</div>
+                                    </div>
+                                    <div class="counting-column">
+                                      <div class="counting-value">${minutes}</div>
+                                      <div class="counting-size">min</div>
+                                    </div>
+                                    <div class="counting-column">
+                                      <div class="counting-value">${seconds}</div>
+                                      <div class="counting-size">sec</div>
+                                    </div>`
+    }
+  }
+  //showRemaining()
+  timer = setInterval(showRemaining, 1000)
+
+  const backgroundImages: Array<string> = [
+    '/praca-da-liberdade.jpeg',
+    '/morar-em-Belo-Horizonte.jpg',
+    '/bhz-viaduto-santa-teresa.jpeg',
+    '/belo-horizonte-pampulha.jpg'
+  ]
+
+  const randomizeBg = () => {
+    const randomIndex = Math.floor(Math.random() * backgroundImages.length)
+    const randomBg = backgroundImages[randomIndex]
+    document.querySelector('.ctdn-bg')?.setAttribute('style', `background-image: url('${randomBg}')`)
+  }
+
+  setInterval(randomizeBg, 10000)
+})
+
+useSeoMeta({
+  title,
+  description: () => `description: ${title.value}`
+})
+
+</script>
+
+<style lang="stylus" scoped>
+.ctdn-bg
+  .ctdn-wrapper
+    z-index 666
+    margin-top -40px
+    .countdown
+      display flex
+      font-size 6rem
+      :deep(.counting-column)
+        display flex
+        flex-direction column
+        width 120px
+        line-height 50px
+      :deep(.counting-size)
+        font-size 1.33rem
+        letter-spacing -1px
+        color rgba(#FFF, 0.6)
+        text-align center
+      :deep(.counting-value)
+        font-size 6rem
+        letter-spacing -3px
+        color rgba(#FFF, 0.9)
+        text-align center
+
+// LIGHT MODE
+body.light
+  .countdown
+    :deep(.counting-size),
+    :deep(.counting-value)
+      color rgba(#282a36, 0.9)
+
+// MEDIA QUERIES
+@media (max-width: 500px)
+  .ctdn-bg
+    .countdown
+      flex-direction column
+      align-items center
+      :deep(.counting-column)
+        margin 10px 0
+</style>
--- pages/next-trip.vue
+++ pages/next-trip.vue
@@ -1,81 +1,11 @@
 <template lang="pug">
 section.ctdn-bg.sec-wh
-  .countdown
+  countdown-clock(
+    :title="'gripp.link | Next Trip Date'"
+    :date="'July 3, 2024 22:45:00'"
+    :wrapperClass="'countdown1'"
+  )
 </template>
-
-<script setup lang="ts">
-import { onMounted } from 'vue'
-const title = ref('gripp.link | Next Trip Date')
-
-onMounted(() => {
-  const countdownElement = document.querySelector('.countdown')
-  const _second: number = 1000
-  const _minute: number = _second * 60
-  const _hour: number = _minute * 60
-  const _day: number = _hour * 24
-  let timer: any
-
-  function showRemaining () {
-    let distance = new Date('June 12, 2024 22:45:00').getTime() - new Date().getTime()
-    let days = Math.floor(distance / _day)
-    let hours = Math.floor((distance % _day) / _hour) < 10 ? '0' + Math.floor((distance % _day) / _hour) : Math.floor((distance % _day) / _hour)
-    let minutes = Math.floor((distance % _hour) / _minute) < 10 ? '0' + Math.floor((distance % _hour) / _minute) : Math.floor((distance % _hour) / _minute)
-    let seconds = Math.floor((distance % _minute) / _second) < 10 ? '0' + Math.floor((distance % _minute) / _second) : Math.floor((distance % _minute) / _second)
-    
-    if (distance < 0) {
-      clearInterval(timer)
-      if (countdownElement) {
-        countdownElement.innerHTML = 'Aguarde a próxima viagem!'
-      }
-    }
-
-    if (countdownElement) {
-      countdownElement.innerHTML = `<div class="counting-column">
-                                      <div class="counting-value">${days}</div>
-                                      <div class="counting-size">days</div>
-                                    </div>
-                                    <div class="counting-column">
-                                      <div class="counting-value">${hours}</div>
-                                      <div class="counting-size">hours</div>
-                                    </div>
-                                    <div class="counting-column">
-                                      <div class="counting-value">${minutes}</div>
-                                      <div class="counting-size">min</div>
-                                    </div>
-                                    <div class="counting-column">
-                                      <div class="counting-value">${seconds}</div>
-                                      <div class="counting-size">sec</div>
-                                    </div>`
-    }
-  }
-  //showRemaining()
-  timer = setInterval(showRemaining, 1000)
-
-  const backgroundImages: Array<string> = [
-    '/praca-da-liberdade.jpeg',
-    '/belo-horizonte-praca-do-papa.jpg',
-    '/morar-em-Belo-Horizonte.jpg',
-    '/praca-da-liberdade2.jpeg',
-    '/morar-em-Belo-Horizonte2.jpeg',
-    '/bhz-viaduto-santa-teresa.jpeg',
-    '/belo-horizonte-pampulha.jpg'
-  ]
-
-  const randomizeBg = () => {
-    const randomIndex = Math.floor(Math.random() * backgroundImages.length)
-    const randomBg = backgroundImages[randomIndex]
-    document.querySelector('.ctdn-bg')?.setAttribute('style', `background-image: url('${randomBg}')`)
-  }
-
-  setInterval(randomizeBg, 10000)
-})
-
-useSeoMeta({
-  title,
-  description: () => `description: ${title.value}`
-})
-
-</script>
 
 <style lang="stylus" scoped>
 .ctdn-bg
@@ -96,43 +26,10 @@ useSeoMeta({
     height 100%
     background rgba(0, 0, 0, 0.67)
     z-index 333
-  .countdown
-    display flex
-    z-index 666
-    font-size 6rem
-    margin-top -40px
-    :deep(.counting-column)
-      display flex
-      flex-direction column
-      width 120px
-      line-height 50px
-    :deep(.counting-size)
-      font-size 1.33rem
-      letter-spacing -1px
-      color rgba(#FFF, 0.6)
-      text-align center
-    :deep(.counting-value)
-      font-size 6rem
-      letter-spacing -3px
-      color rgba(#FFF, 0.9)
-      text-align center
 
 // LIGHT MODE
 body.light
   .ctdn-bg
     &:before
       background rgba(211, 211, 211, 0.66)
-  .countdown
-    :deep(.counting-size),
-    :deep(.counting-value)
-      color rgba(#282a36, 0.9)
-
-// MEDIA QUERIES
-@media (max-width: 500px)
-  .ctdn-bg
-    .countdown
-      flex-direction column
-      align-items center
-      :deep(.counting-column)
-        margin 10px 0
 </style>
